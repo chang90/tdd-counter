adab0cfc337cbb24da479c384fef958254630579
hiby90hou <chang.liu.programmer@gmail.com>
2021-03-03 23:11:37 +1100

    add TDD counter

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,34 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { mount, ReactWrapper } from 'enzyme';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('Counter Testing', () => {
+  let wrapper: ReactWrapper;
+  beforeEach(() => {
+    wrapper = mount(<App />)
+  });
+
+  test('render the title of counter', () => {
+    expect(wrapper.find('h1').text()).toContain('This is counter app')
+  });
+
+  test('render a button with text of `increment`', () => {
+    expect(wrapper.find('#increment-btn').text()).toBe('Increment')
+  })
+
+  test('render the initial value of state in a div', () => {
+    expect(wrapper.find('#counter-value').text()).toBe('0');
+  })
+
+  test('render the click event of increment button and increment counter value', () => {
+    wrapper.find('#increment-btn').simulate('click');
+    expect(wrapper.find('#counter-value').text()).toBe('1');
+  })
+
+  test('render the click event of decrement button and decrement counter value', () => {
+    wrapper.find('#increment-btn').simulate('click');
+    expect(wrapper.find('#counter-value').text()).toBe('1');
+    wrapper.find('#decrement-btn').simulate('click');
+    expect(wrapper.find('#counter-value').text()).toBe('0');
+  })
+})
